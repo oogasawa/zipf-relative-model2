e94625e8ff631cced2d38640f79ceff29fe1985b
Osamu Ogasawara <oogasawa@nig.ac.jp>
2019-05-06 14:32:52 +0900

    Bug fixed

--- prototype3/SimulationActor.js
+++ prototype3/SimulationActor.js
@@ -40,7 +40,7 @@ class Parameter {
 		// simulation parameters.
 		this.minRna   = 1.0;
 		this.totalRna = 300000;
-		this.numOfGenes = 10000;
+		this.numOfGenes = 20000;
 
 		this.standardDeviation  = 0.01;
 		this.propOfMutatedGenes = 0.001;
